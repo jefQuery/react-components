bea6cb9fc81e02434980eb37f035233a9bfbb414
jefQuery <jeffrey.milberger@gmail.com>
2017-02-22 15:34:22 -0800

    complete the minisprint requirements

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-// TODO
+// import React from 'react';
+
+// var Cucumbers = () => (
+//   <li>Cucumbers</li>
+// );
+
+// var Kale = () => (
+//   <li>Kale</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hover: false
+    }
+  }
+  render(props){
+    var onListItemHover = (event) => {
+      console.log('lol');
+      this.setState({
+        hover: !this.state.hover
+      });
+    }
+    var onListItemClick = (event) => {
+      console.log('I got clickedz!');
+    };
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+    return(
+      <li style={style} onMouseOver={onListItemHover.bind(this)} onClick={onListItemClick}>{this.props.food.name}</li>
+    );
+  }
+  
+};
+
+var GroceryList = (props) => ( 
+  <div>
+    <h2>My Grocery List</h2>
+      <ul>
+      {props.groceries.map((grocery, i) =>
+        <GroceryListItem key={i} food={grocery}/>
+      )}
+      </ul>
+  </div>
+);
+
+var GROCERIES = [{name:'cucumbers'}, {name:'kale'}, {name:'watermelon'}, {name:'SOYLENT'}]
+ReactDOM.render(<GroceryList groceries={GROCERIES} />, document.getElementById('app'));
